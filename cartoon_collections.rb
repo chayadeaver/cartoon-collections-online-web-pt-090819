fb87e5b3aa153aee2bb3bccd58a081aa69007697
Chaya Deaver <chayadeaver@gmail.com>
2019-09-13 21:11:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   array.each_with_index do |name, index|
     i = index + 1
-    puts "#{i}. name"
+    puts "#{i}. #{name}"
   end# Your code here
 end
 

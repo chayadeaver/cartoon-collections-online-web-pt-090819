bd87af652b073d610f55b936d6bd28a7b8dd09ef
Chaya Deaver <chayadeaver@gmail.com>
2019-09-13 21:55:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,11 +12,12 @@ end
 def long_planeteer_calls(array)
   array.each do |calls|
     array.any? do |calls|
-      if calls.length > 4
-      return true
-    else
-      return false
+      calls.length > 4
+        return true
     end
+    array.all? do |calls|
+      calls.length < 4
+      return false
     end
   end# Your code here
 end

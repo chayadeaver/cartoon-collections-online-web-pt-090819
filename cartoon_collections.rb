7a09ff72a9cebe045c0ab571023ee22707471e3c
Chaya Deaver <chayadeaver@gmail.com>
2019-09-13 23:08:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0# code an argument here
   array.find(cheese_types[i]) do
-    cheese_types[i]
+    array[i]
   end# the array below is here to help
 end

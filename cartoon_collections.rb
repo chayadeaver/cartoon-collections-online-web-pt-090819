6cefb84ec11350a9697b3e9020ef42e9db64e1e8
Chaya Deaver <chayadeaver@gmail.com>
2019-09-13 22:38:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0# code an argument here
   array.include?(cheese_types[i]) do
     array.first
   end# the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
 end

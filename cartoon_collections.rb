3be41fffd9fc5a7e28baf33da5885439356c150a
Chaya Deaver <chayadeaver@gmail.com>
2019-09-13 21:11:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index do |name, index|
+    i = index + 1
+    puts "#{i}. name"
+  end# Your code here
 end
 
 def summon_captain_planet# code an argument here

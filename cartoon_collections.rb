b8a803d88724e0ac6327f01d4581d1df713138ae
Chaya Deaver <chayadeaver@gmail.com>
2019-09-13 22:34:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(array)
     end# Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)# code an argument here
+  array.find?{|cheese|}# the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end

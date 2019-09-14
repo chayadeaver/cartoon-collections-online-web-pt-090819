049bcfe7dc1d758ea3f4cd1c803e69dd740c26e3
Chaya Deaver <chayadeaver@gmail.com>
2019-09-14 01:30:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,9 +8,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)
-    array.any? do |calls|
-       calls.length > 4
-    end# Your code here
+    array.any? {|calls| calls.length > 4} # Your code here
 end
 
 def find_the_cheese(array)

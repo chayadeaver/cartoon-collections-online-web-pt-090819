a460337a608bf889ab1254343805407337295dcb
Chaya Deaver <chayadeaver@gmail.com>
2019-09-14 00:59:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,5 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0# code an argument here
   array.find {|cheese| array.include?(cheese_types[i])}
-  array.[i]
 
 end
